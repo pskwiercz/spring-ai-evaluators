7d52b4993fe9c608ee7f6edb72f11f1358ae44ca
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2025-12-24 11:47:53 +0100

    Runtime validation responses

--- src/main/java/com/pskwiercz/evaluators/SpringAiEvaluatorsApplication.java
+++ src/main/java/com/pskwiercz/evaluators/SpringAiEvaluatorsApplication.java
@@ -2,8 +2,10 @@ package com.pskwiercz.evaluators;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class SpringAiEvaluatorsApplication {
 
     public static void main(String[] args) {
